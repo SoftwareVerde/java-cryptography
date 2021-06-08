1c6742967298ccf3df94f9a25a9a827edbc74912
Andrew Groot <groot@softwareverde.com>
2021-06-08 11:40:44 -0400

    Adjusting public key recovery algorithm to expect a specific recovery ID.

--- src/main/java/com/softwareverde/cryptography/secp256k1/key/PublicKey.java
+++ src/main/java/com/softwareverde/cryptography/secp256k1/key/PublicKey.java
@@ -38,89 +38,71 @@ public class PublicKey extends ImmutableByteArray implements Const {
     /**
      * Determine the public key used from a signed message.
      *  This algorithm operation is described in sec1-v2, section 4.1.6: https://www.secg.org/sec1-v2.pdf
-     *  If provided, `recoveryIndex` is used to seed the recoveryIndex, and its value is updated to the j-index used by the algorithm.
+     *  The `recoveryIndex` takes the place of the `j` and `k` variables in the specification, allowing the calling
+     *  function to either provide a known recovery ID or iterate through the possibilities (0-3) to find a public key
+     *  candidate that meets criteria known to the caller.
      */
-    public static PublicKey fromSignature(final Signature signature, final Sha256Hash messagePreImage, final Container<Integer> recoveryIndex) {
+    public static PublicKey fromSignature(final Signature signature, final Sha256Hash messagePreImage, final Integer recoveryIndex) {
         final ECCurve curve = Secp256k1.CURVE_DOMAIN.getCurve();
         final BigInteger n = Secp256k1.CURVE_DOMAIN.getN();  // Curve order.
         final BigInteger r = new BigInteger(1, signature.getR().getBytes());
         final BigInteger s = new BigInteger(1, signature.getS().getBytes());
 
-        final int startIndex;
+        //   1.1 Let x = r + jn
+        final BigInteger i = BigInteger.valueOf(recoveryIndex / 2L);
+        final BigInteger x = r.add(i.multiply(n));
+
+        // 1.2. Convert the integer x to an octet string X of length mlen using the conversion routine
+        //      specified in Section 2.3.7, where mlen = d(log2 p)/8e or mlen = dm/8e.
+        // 1.3. Convert the octet string 0216kX to an elliptic curve point R using the conversion routine
+        //      specified in Section 2.3.4. If this conversion routine outputs "invalid", then do another
+        //      iteration of Step 1.
+        if (x.compareTo(Secp256k1.CURVE_P) >= 0) { return null; } // Public key is not on the curve.
+
+        final byte[] compressedPoint = new byte[32 + 1];
         {
-            final boolean useRecoveryIndex = ( (recoveryIndex != null) && (recoveryIndex.value >= 0) && (recoveryIndex.value < 4) );
-            if (useRecoveryIndex) {
-                startIndex = recoveryIndex.value;
-            }
-            else {
-                startIndex = 0;
-            }
+            final boolean yCoordinateIsEven = ((recoveryIndex & 0x01) == 0x00);
+
+            final byte[] xPointBytes = x.toByteArray();
+            compressedPoint[0] = (byte) (yCoordinateIsEven ? 0x02 : 0x03);
+            ByteUtil.setBytes(compressedPoint, ByteUtil.getTailBytes(xPointBytes, 32), 1);
         }
 
-        // 1. For j from 0 to h do the following.
-        for (int j = startIndex; j < 4; ++j) {
-            //   1.1 Let x = r + jn
-            final BigInteger i = BigInteger.valueOf(j / 2L);
-            final BigInteger x = r.add(i.multiply(n));
+        final byte[] decompressedPublicKeyBytes = Secp256k1.decompressPoint(compressedPoint);
+        if (decompressedPublicKeyBytes == null) { return null; }
 
-            // 1.2. Convert the integer x to an octet string X of length mlen using the conversion routine
-            //      specified in Section 2.3.7, where mlen = d(log2 p)/8e or mlen = dm/8e.
-            // 1.3. Convert the octet string 0216kX to an elliptic curve point R using the conversion routine
-            //      specified in Section 2.3.4. If this conversion routine outputs "invalid", then do another
-            //      iteration of Step 1.
-            if (x.compareTo(Secp256k1.CURVE_P) >= 0) { continue; } // Public key is not on the curve.
+        final ECPoint publicKeyPoint = curve.decodePoint(decompressedPublicKeyBytes);
 
-            final byte[] compressedPoint = new byte[32 + 1];
-            {
-                final boolean yCoordinateIsEven = ((j & 0x01) == 0x00);
+        // 1.4. If nR != infinity, then do another iteration of Step 1.
+        if (! publicKeyPoint.multiply(n).isInfinity()) { return null; }
 
-                final byte[] xPointBytes = x.toByteArray();
-                compressedPoint[0] = (byte) (yCoordinateIsEven ? 0x02 : 0x03);
-                ByteUtil.setBytes(compressedPoint, ByteUtil.getTailBytes(xPointBytes, 32), 1);
-            }
+        // 1.5. Compute e from M using Steps 2 and 3 of ECDSA signature verification.
+        final BigInteger e = new BigInteger(1, messagePreImage.getBytes());
 
-            final byte[] decompressedPublicKeyBytes = Secp256k1.decompressPoint(compressedPoint);
-            if (decompressedPublicKeyBytes == null) { continue; }
+        // 1.6. For k from 1 to 2 do the following.
+        //   1.6.1. Compute a candidate public key as:
+        //          Q = r^(−1)(sR − eG)
 
-            final ECPoint publicKeyPoint = curve.decodePoint(decompressedPublicKeyBytes);
+        // Thanks to Google/Andreas Schildbach:
+        //      Q = r^(−1)(sR − eG) = mi(r) * (sR - eG)
+        // Where mi(x) is the modular multiplicative inverse.
+        // Therefore, Q = (mi(r) * s ** R) + (mi(r) * -e ** G)
+        // Where -e is the modular additive inverse of e, that is z such that z + e = 0 (mod n).
+        // Where "**" and "+" is EC maths, aka point multiplication and point addition, respectively.
+        //
+        // To find the additive inverse, subtract e from zero then take the mod.
+        // For example, the additive inverse of 3 modulo 11 is 8 because 3 + 8 mod 11 = 0, and -3 mod 11 = 8.
 
-            // 1.4. If nR != infinity, then do another iteration of Step 1.
-            if (! publicKeyPoint.multiply(n).isInfinity()) { continue; }
+        final BigInteger eInverse = BigInteger.ZERO.subtract(e).mod(n);
+        final BigInteger rInverse = r.modInverse(n);
+        final BigInteger srInverse = rInverse.multiply(s).mod(n);
+        final BigInteger eInverseTimesRInverseModN = rInverse.multiply(eInverse).mod(n);
+        final ECPoint q = ECAlgorithms.sumOfTwoMultiplies(Secp256k1.CURVE_DOMAIN.getG(), eInverseTimesRInverseModN, publicKeyPoint, srInverse);
 
-            // 1.5. Compute e from M using Steps 2 and 3 of ECDSA signature verification.
-            final BigInteger e = new BigInteger(1, messagePreImage.getBytes());
+        final byte[] publicKeyBytes = q.getEncoded(false);
+        if (publicKeyBytes.length == 1) { return null; }
 
-            // 1.6. For k from 1 to 2 do the following.
-            //   1.6.1. Compute a candidate public key as:
-            //          Q = r^(−1)(sR − eG)
-
-            // Thanks to Google/Andreas Schildbach:
-            //      Q = r^(−1)(sR − eG) = mi(r) * (sR - eG)
-            // Where mi(x) is the modular multiplicative inverse.
-            // Therefore, Q = (mi(r) * s ** R) + (mi(r) * -e ** G)
-            // Where -e is the modular additive inverse of e, that is z such that z + e = 0 (mod n).
-            // Where "**" and "+" is EC maths, aka point multiplication and point addition, respectively.
-            //
-            // To find the additive inverse, subtract e from zero then take the mod.
-            // For example, the additive inverse of 3 modulo 11 is 8 because 3 + 8 mod 11 = 0, and -3 mod 11 = 8.
-
-            final BigInteger eInverse = BigInteger.ZERO.subtract(e).mod(n);
-            final BigInteger rInverse = r.modInverse(n);
-            final BigInteger srInverse = rInverse.multiply(s).mod(n);
-            final BigInteger eInverseTimesRInverseModN = rInverse.multiply(eInverse).mod(n);
-            final ECPoint q = ECAlgorithms.sumOfTwoMultiplies(Secp256k1.CURVE_DOMAIN.getG(), eInverseTimesRInverseModN, publicKeyPoint, srInverse);
-
-            final byte[] publicKeyBytes = q.getEncoded(false);
-            if (publicKeyBytes.length == 1) { return null; }
-
-            if (recoveryIndex != null) {
-                recoveryIndex.value = j;
-            }
-
-            return new PublicKey(publicKeyBytes);
-        }
-
-        return null;
+        return new PublicKey(publicKeyBytes);
     }
 
     protected Boolean _isCompressed() {
